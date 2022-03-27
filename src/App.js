f01c958fc55469e14e148e4d7ac73a2ebd5b3f56
Bob Kearns <kearnsr135@gmail.com>
2022-03-27 15:34:10 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const API = {
+  key: "d32739f7b50e5b10888e6d3d9c437154",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function App() {
+  return (
+    <div className="App">
+        hello
+    </div>
+  );
+}
+
+export default App;
